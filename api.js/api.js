d7b593e54abf9a2add788dccfa9d92fec6446f45
Rudraranjan Thakur <rudraranjan.thakur@gmail.com>
2021-09-02 11:43:01 +0530

    before adding search result amount

--- api.js/api.js
+++ api.js/api.js
@@ -1,3 +1,6 @@
+const displayMassage = document.getElementById('display error');
+displayMassage.style.display = 'none';
+
 // search result fetching data
 
 const searchButton = () => {
@@ -11,24 +14,36 @@ const searchButton = () => {
 
 const displayBooks = (items) => {
     console.log(items);
-    const itemPush =document.getElementById('items-push');
-    items.forEach(item => {
-    const div = document.createElement('div')
-    div.classList.add('col')
-    div.innerHTML= `
-             <div class="card">
-                 <img src="..." class="card-img-top" alt="...">
-                 <div class="card-body">
-                    <h5 class="card-title">Book Name :${item.title}</h5>
-                    <p>Author Name : ${item.author_name}</p>
-                    <p>First Publishing : ${item.first_publish_year ? item.first_publish_year: 'Not Available'}</p>
-                    <p>Publishers : ${item.publisher[0] ? item.publisher[0]: 'Publisher Details Not Available'} </p>
-                    
-                 </div>
-             </div
+    const itemPush =document.getElementById('items-push'); 
+    itemPush.textContent ='';    
     
-    `
-     itemPush.appendChild(div);   
-    });
+    if(items.length === 0){
+        const displayMassage = document.getElementById('display error');
+        displayMassage.style.display = 'block';
+    }
+    else{
+        items.forEach(item => {        
+            const div = document.createElement('div')
+            div.classList.add('col')
+            const url =`https://covers.openlibrary.org/b/id/${item.cover_i}-L.jpg`
+            div.innerHTML= `
+                     <div class="card">
+                         <img src="${url}"class="w-50 m-auto card-img-top" alt="...">
+                         <div class="card-body">
+                            <h5 class="card-title">Book Name :${item.title ? item.title: 'Book Name Not Available'}</h5>
+                            <p>Author Name : ${item.author_name ? item.author_name: 'Author Name Not Available' }</p>
+                            <p>First Publishing : ${item.first_publish_year ? item.first_publish_year: 'Not Available'}</p>
+                            <p>Publishers : ${item.publisher ? item.publisher: 'Publisher Details Not Available'} </p>
+                            
+                         </div>
+                     </div
+            
+            `
+             itemPush.appendChild(div); 
+             const displayMassage = document.getElementById('display error');
+            displayMassage.style.display = 'none';  
+            });
+    }
+   
     
 }
